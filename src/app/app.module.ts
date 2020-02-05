44d4b876bc932dccddc0dfdb6110b012536f27b9
Gaelen Mcintee <gaelen.mcintee@gfs.com>
2020-02-05 08:54:37 -0500

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import {RouterModule, Routes} from '@angular/router';
+import { TriviaComponent } from './trivia/trivia.component';
+import { PollsComponent } from './polls/polls.component';
+import { ChallengesComponent } from './challenges/challenges.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routes: Routes = [
+  {
+    path: 'trivia',
+    component: TriviaComponent,
+  },
+  {
+    path: 'polls',
+    component: PollsComponent
+  },
+  {
+    path: 'challenges',
+    component: ChallengesComponent
+  },
+  {
+    path: 'profile/:id',
+    component: ProfileComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TriviaComponent,
+    PollsComponent,
+    ChallengesComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
